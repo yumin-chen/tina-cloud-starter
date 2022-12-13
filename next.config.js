905df5c7c172c0a38d505f52ef95790389fc46aa
James O'Halloran <james.p.ohalloran@gmail.com>
2022-12-13 11:04:49 -0400

    allow links to work to specific pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 module.exports = {
   webpack(config) {
     config.module.rules.push({
@@ -20,24 +22,45 @@ module.exports = {
           destination: "/admin/index.html",
         },
       ],
-      afterFiles: [
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "header",
-              key: "sec-fetch-dest",
-              value: "document",
-            },
-            {
-              type: "cookie",
-              key: "tina_auth",
-              value: "true",
-            },
-          ],
-          destination: "http://localhost:3000/admin#/~/:path*",
-        },
-      ],
+      // afterFiles: [
+      //   {
+      //     source: "/:path*",
+      //     has: [
+      //       {
+      //         type: "header",
+      //         key: "sec-fetch-dest",
+      //         value: "document",
+      //       },
+      //       {
+      //         type: "cookie",
+      //         key: "tina_auth",
+      //         value: "true",
+      //       },
+      //     ],
+      //     destination: "http://localhost:3000/admin?adminroute=#/~/:path*",
+      //   },
+      // ],
     };
+  },
+  async redirects() {
+    return [
+      {
+        has: [
+          {
+            type: "header",
+            key: "sec-fetch-dest",
+            value: "document",
+          },
+          {
+            type: "cookie",
+            key: "tina_auth",
+            value: "true",
+          },
+        ],
+        source: "/:path((?!admin$).*)",
+        destination: "/admin?redirect=/#/~/:path*",
+        permanent: false,
+      },
+    ];
   },
 };
